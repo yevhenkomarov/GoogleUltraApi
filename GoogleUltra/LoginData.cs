c1bab3000e7225b7299534a2e5a75e6de2d6c758
yevhenkomarov <yevhenkomarov@gmail.com>
2020-12-04 10:03:19 +0200

    Delete login data

--- GoogleUltra/LoginData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GoogleUltra
-{
-    public static class LoginData
-    {
-        public const string Login = "yevhenkomarov@gmail.com";
-        public const string Password = "MkvPcb18";
-    }
-}
